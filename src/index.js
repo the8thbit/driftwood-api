47f655eef3cfb2e988c28d9f1c48dd7fdffc2ee9
the8thbit <the8thbit@gmail.com>
2020-09-26 23:54:35 -0500

    switch to es6+ variable declarations

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const normalizeUrl = require("normalize-url");
 const app = express();
 
 const validURL = (str) => {
-  var pattern = new RegExp(
+  let pattern = new RegExp(
     '^(https?:\\/\\/)?'+ // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
