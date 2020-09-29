410ca9889d8ee36f062552a02775358ff5ade766
the8thbit <the8thbit@gmail.com>
2020-09-28 22:43:33 -0500

    fix sql module pathing issue

--- src/sql.js
+++ src/sql.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const sqlPath = './sql'
+const sqlPath = 'src/sql'
 
 const getSqlStr = (sqlFilePath) => {
     return fs.readFileSync(`${sqlPath}/${sqlFilePath}.sql`).toString();
